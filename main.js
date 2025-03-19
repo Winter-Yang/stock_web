26a37a4dcc40ac8197998a47b94ae70f2b023b4d
yangwende <yangwende@zuoyebang.com>
2025-03-19 20:20:23 +0800

    opt code

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
-const app = express();
+const express = require('express');
 const https = require('https');
+const cors = require('cors');
+const path = require('path');
 
-
+const app = express();
 app.use(cors());
-
-
 //start app 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8233;
 
 app.listen(port, async () => {
     console.log(`App is listening on port:${port}. you can open if run test http://localhost:${port}/`);
   }
 );
 
-
-
-app.get('/api/cdn/domainlist',function (req, res){
-    try {
-        // 解析 JSON 字符串
-        const jsonData = JSON.parse(data);
-        
-        // 添加自定义 header
-        res.setHeader('X-Cdn-Version', 'a0bc1f0edff7471db41f58d0da0d9c40'); // 设置 X-Cdn-Version 头
-        
-        // 返回 JSON 数据
-        res.json(jsonData);
-    } catch (parseErr) {
-        // 处理 JSON 解析错误
-        res.status(400).json({ error: 'Invalid JSON format' });
-    }
-})
+// 处理其他所有路由，返回 index.html
+app.use(express.static(path.join(__dirname,'web', 'YHybrid')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'web', 'YHybrid', 'index.html'));
+});
+// 处理所有路由请求
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'web','YHybrid', 'index.html'));
+});
 
